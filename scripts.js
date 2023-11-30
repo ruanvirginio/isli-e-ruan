2847f379f3d1c3f2f95f8216d9b50c7259fdaffe
Ruan Virginio <49958559+ruanvirginio@users.noreply.github.com>
2023-11-30 14:53:41 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 function openEnvelope() {
-    var envelope = document.getElementById('envelope');
-    var invitation = document.getElementById('invitation');
-  
-    // Troca a imagem do envelope aberto
-    envelope.style.backgroundImage = "url('envelope-aberto.jpg')"; /* Imagem do envelope aberto */
-  
-    // Exibe o conteúdo do convite
-    invitation.style.display = 'block';
-  }
-  
+  var envelope = document.getElementById('envelope');
+
+  // Troca a imagem do envelope aberto
+  envelope.style.backgroundImage = "url('envelope-aberto.jpg')"; /* Imagem do envelope aberto */
+
+  // Após 5 segundos, abrir a nova aba
+  setTimeout(function () {
+    window.open('https://noivos.casar.com/isli-e-ruan', '_blank');
+  }, 5000); // 5000 milissegundos = 5 segundos
+}
